68c65102e35e737f8819fd8540efa930f4a92e6c
Fernando Jr <jr.fernandodasilva@gmail.com>
2024-05-23 00:03:10 -0300

    refactor(main.js): used alias path syntax "@"

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 import '@/assets/css/main.css'
-import store from './store'
-import i18n from './plugins/i18n';
+import store from '@/store'
+import i18n from '@/plugins/i18n';
 
 createApp(App)
   .use(store)
