1c0cf6ee816d73131d39c4753165ba9b4ba7049f
Fernando Jr <jr.fernandodasilva@gmail.com>
2024-05-23 00:00:36 -0300

    refactor(i18n): plugin moved to plugins folder to better standardization

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '@/assets/css/main.css'
 import store from './store'
-import i18n from './i18n';
+import i18n from './plugins/i18n';
 
 createApp(App)
   .use(store)
