33fa7259ae15cf8aa785daf17466fb8f8741d88c
Fernando Jr <jr.fernandodasilva@gmail.com>
2024-05-22 23:01:37 -0300

    chore(vue config): added "publicPath" config

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const webpack = require('webpack');
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue3-todo/' : '/',
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
