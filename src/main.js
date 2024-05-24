5710b1533e1eecfe99754e7e1aa6e62449782e07
Fernando Jr <jr.fernandodasilva@gmail.com>
2024-05-23 23:20:56 -0300

    chore(notification): lib vue3-notification configured globally

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from '@/App.vue'
 import '@/assets/css/main.css'
 import store from '@/store'
 import i18n from '@/plugins/i18n';
+import Notifications from '@kyvg/vue3-notification'
 
 createApp(App)
   .use(store)
   .use(i18n)
+  .use(Notifications)
   .mount('#app')
