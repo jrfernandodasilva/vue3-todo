edd4f5c97079f4e23ac54be30e0dff5e29f1158d
Fernando Jr <jr.fernandodasilva@gmail.com>
2024-05-26 23:05:09 -0300

    refactor(todo/main): syntax changed to script setup notation

--- src/components/Todo/TodoMain.vue
+++ src/components/Todo/TodoMain.vue
@@ -5,19 +5,9 @@
   <TodoEmpty v-if="!$store.state.todo.all.length"/>
 </template>
 
-<script>
+<script setup>
 import TodoFormAdd from '@/components/Todo/FormAdd'
 import TodoFilter from '@/components/Todo/Filter'
 import TodoItems from '@/components/Todo/Items'
 import TodoEmpty from '@/components/Todo/Empty'
-
-export default {
-  name: 'TodoMain',
-  components: {
-    TodoFormAdd,
-    TodoFilter,
-    TodoItems,  
-    TodoEmpty
-  }
-}
 </script>
