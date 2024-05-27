842011c36effeb53e71db42dd40d9b34db3f2c83
Fernando Jr <jr.fernandodasilva@gmail.com>
2024-05-26 21:30:30 -0300

    refactor(app): syntax changed to script setup notation

--- src/App.vue
+++ src/App.vue
@@ -4,43 +4,29 @@
   <div class="px-3 py-10 md:px-10">
     <div class="w-full sm:w-5/3 md:w-4/5 lg:w-2/3 mx-auto">
       <LanguageSelector/>
-      <TodoSpinner v-if="loading"/>
+      <Spinner v-if="loading"/>
       <Todo v-else/>   
     </div>
   </div>
 </template>
 
-<script>
-
+<script setup>
 import { reactive, toRefs } from 'vue'
 import { useStore } from 'vuex'
-import TodoSpinner from '@/components/ui/Spinner'
+import Spinner from '@/components/ui/Spinner'
 import LanguageSelector from '@/components/layout/LanguageSelector'
 import Todo from '@/components/Todo/TodoMain'
-
-export default {
-  name: 'App',
-  components: { 
-    TodoSpinner,
-    LanguageSelector,
-    Todo 
-  },
-  setup () {
-
-    const state = reactive({
-      loading: true
-    }) 
-
-    const store = useStore()
-    store.dispatch('todo/all').finally(() => {
-      setTimeout(() => {  // simulate load time
-         state.loading = false;
-        }, store.state.simulateLoadTime)
-    })
  
-    return {
-      ...toRefs(state)
-    }
-  }  
-}
+const state = reactive({
+  loading: true
+}) 
+
+const store = useStore()
+store.dispatch('todo/all').finally(() => {
+  setTimeout(() => {  // simulate load time
+      state.loading = false;
+    }, store.state.simulateLoadTime)
+})
+
+const { loading } = toRefs(state)
 </script>
