5ec0235ed04d59a0418af9eee1a34126241d6ddc
Fernando Jr <jr.fernandodasilva@gmail.com>
2024-05-26 22:56:54 -0300

    refactor(language selector): syntax changed to script setup notation

--- src/components/layout/LanguageSelector.vue
+++ src/components/layout/LanguageSelector.vue
@@ -8,44 +8,35 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import { computed, reactive, toRefs } from 'vue';
 import { useStore } from 'vuex';
 import { useI18n } from 'vue-i18n'
 import { useCapitalize } from '@/composables/capitalize'
 import { useNotification } from '@kyvg/vue3-notification'
 
-export default {
-  name: 'LanguageSelector',
-  setup() {
-    const store = useStore();
-    const { t } = useI18n()
-    const { notify } = useNotification()
+const store = useStore();
+const { t } = useI18n()
+const { notify } = useNotification()
 
-    const state = reactive({
-      locales: store.state.locales,
-      capitalize: useCapitalize
-    })
+const state = reactive({
+  locales: store.state.locales,
+  capitalize: useCapitalize
+})
 
-    const currentLocale = computed({
-      get() {
-        return store.state.locale;
-      },
-      set(value) {
-        store.dispatch('updateLocale', value)
-        .then(() => notify({ group:'global', type:'info', title: useCapitalize(t('success')), text:t('language changed with success')}) )        
-      },
-    })
-
-    const changeLanguage = (event) => {
-      currentLocale.value = event.target.value;
-    }
-
-    return {
-      ...toRefs(state),
-      currentLocale,
-      changeLanguage,
-    };
+const currentLocale = computed({
+  get() {
+    return store.state.locale;
   },
-};
+  set(value) {
+    store.dispatch('updateLocale', value)
+    .then(() => notify({ group:'global', type:'info', title: useCapitalize(t('success')), text:t('language changed with success')}) )        
+  },
+})
+
+const changeLanguage = (event) => {
+  currentLocale.value = event.target.value;
+}
+
+const { locales, capitalize } = toRefs(state)
 </script>
