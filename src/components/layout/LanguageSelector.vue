fd93ce705b2be92d61c9433052480212457e4cc0
Fernando Jr <jr.fernandodasilva@gmail.com>
2024-05-26 22:32:45 -0300

    chore(component): adjust to show select language better in the header

--- src/components/layout/LanguageSelector.vue
+++ src/components/layout/LanguageSelector.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed top-4 right-4">
+  <div class="fixed right-6">
     <select v-model="currentLocale" @change="changeLanguage" class="capitalize bg-gray-900 border border-gray-700 text-gray-100 py-2 px-4 rounded shadow">
       <option v-for="(locale, key) in locales" :key="key" :value="key">
         {{ capitalize($t(locale.title)) }}
