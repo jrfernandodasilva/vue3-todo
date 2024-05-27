72cf2b4c01eff61b4c09a3ea5ff9e43715c3ce48
Fernando Jr <jr.fernandodasilva@gmail.com>
2024-05-26 22:53:55 -0300

    chore(language): added global notification feedback on change language

--- src/components/layout/LanguageSelector.vue
+++ src/components/layout/LanguageSelector.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="fixed right-6">
-    <select v-model="currentLocale" @change="changeLanguage" class="capitalize bg-gray-900 border border-gray-700 text-gray-100 py-2 px-4 rounded shadow">
+    <select @change="changeLanguage" class="capitalize bg-gray-900 border border-gray-700 text-gray-100 py-2 px-4 rounded shadow">
       <option v-for="(locale, key) in locales" :key="key" :value="key">
         {{ capitalize($t(locale.title)) }}
       </option>
@@ -11,12 +11,16 @@
 <script>
 import { computed, reactive, toRefs } from 'vue';
 import { useStore } from 'vuex';
+import { useI18n } from 'vue-i18n'
 import { useCapitalize } from '@/composables/capitalize'
+import { useNotification } from '@kyvg/vue3-notification'
 
 export default {
   name: 'LanguageSelector',
   setup() {
     const store = useStore();
+    const { t } = useI18n()
+    const { notify } = useNotification()
 
     const state = reactive({
       locales: store.state.locales,
@@ -28,7 +32,8 @@ export default {
         return store.state.locale;
       },
       set(value) {
-        store.dispatch('updateLocale', value);
+        store.dispatch('updateLocale', value)
+        .then(() => notify({ group:'global', type:'info', title: useCapitalize(t('success')), text:t('language changed with success')}) )        
       },
     })
 
