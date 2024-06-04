7126836ca6c9377f91396c964e56a452b29bc025
Fernando Jr <jr.fernandodasilva@gmail.com>
2024-06-04 19:34:21 -0300

    fix(publicPath): define app base url correctly by env

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const webpack = require('webpack');
 
 module.exports = defineConfig({
-  // publicPath: process.env.GITHUB_ACTIONS === 'true' ? '/vue3-todo/' : '/',
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue3-todo/' : '/',
+  publicPath: process.env.BASE_URL ?? '/',
   transpileDependencies: true,
   configureWebpack: {
     watchOptions: {
