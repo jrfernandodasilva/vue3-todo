b9e565944a9c3722736ebffc9c22b2b3bb5d2828
Fernando Jr <jr.fernandodasilva@gmail.com>
2024-06-04 18:53:40 -0300

    chore(publicPath): configured publicPath based in GITHUB_ACTIONS

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const webpack = require('webpack');
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue3-todo/' : '/',
+  publicPath: process.env.GITHUB_ACTIONS === 'true' ? '/vue3-todo/' : '/',
   transpileDependencies: true,
   configureWebpack: {
     watchOptions: {
